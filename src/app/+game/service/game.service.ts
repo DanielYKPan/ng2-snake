db2e6bbee5e140f6dcc6a39e203eb29ff4962e1d
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-26 21:11:56 +1100

    Added Fn BuildGridWithWalls

--- src/app/+game/service/game.service.ts
+++ src/app/+game/service/game.service.ts
@@ -3,15 +3,33 @@
  */
 
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Tile, TileContent } from './tile';
+
+const GameStatic: any = {
+    columns: 20,
+    rows: 15,
+    tileWidth: 32,
+    tileHeight: 32
+};
 
 @Injectable()
 export class GameService {
 
-    constructor() {
+    constructor( private store: Store<any> ) {
     }
 
-    public buildGrid(): void {
-        
+    public buildGridWithWalls(): void {
+        let tiles: Tile[] = [];
+        for (let i = 0; i < GameStatic.columns; i++) {
+            for (let j = 0; j < GameStatic.rows; j++) {
+                if (i === 0 || i === GameStatic.columns - 1 || j === 0 || j === GameStatic.rows - 1) {
+                    tiles[i][j] = new Tile(TileContent.Wall);
+                } else {
+                    tiles[i][j] = new Tile(TileContent.Empty);
+                }
+            }
+        }
     }
 
 }
