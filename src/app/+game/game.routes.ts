67183661f5780245050dca8e11afed4a84c9cb88
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-01 16:02:38 +1100

    Create route to GameBoard:

--- src/app/+game/game.routes.ts
+++ src/app/+game/game.routes.ts
@@ -3,12 +3,14 @@
  */
 
 import { GameComponent } from './game.component';
+import { GameBoardComponent } from './game-board';
 
 export const routes = [
     {
         path: '',
+        component: GameComponent,
         children: [
-            {path: '', component: GameComponent},
+            {path: '', component: GameBoardComponent},
         ]
     },
 ];
