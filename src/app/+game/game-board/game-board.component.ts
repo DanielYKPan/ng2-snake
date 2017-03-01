4f916c5aaae36776827d69bd9681b68a0e692327
Yaokun Pan <myron.yk.pan@gmail.com>
2017-03-01 15:59:36 +1100

    Move game-inform to game-board

--- src/app/+game/game-board/game-board.component.ts
+++ src/app/+game/game-board/game-board.component.ts
@@ -3,25 +3,37 @@
  */
 
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
-import { GameService, KeyboardService, KeyCode } from '../service';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { GameService, KeyboardService, KeyCode, IGameState, Fruit, FruitType } from '../service';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
     selector: 'app-game-board',
     templateUrl: 'game-board.component.html',
-    //styleUrls: ['./game-board.component.scss'],
 })
 export class GameBoardComponent implements OnInit, OnDestroy {
+
+    public gameState: Observable<IGameState>;
+    public fruits: Array<{name: string, score: number}>;
 
     @ViewChild('board') private board: ElementRef;
 
     private arrowsSub: Subscription;
 
     constructor( private gameService: GameService,
-                 private keyboardService: KeyboardService ) {
+                 private keyboardService: KeyboardService,
+                 private store: Store<any> ) {
     }
 
     public ngOnInit() {
+
+        this.gameState = this.store.select('gameState');
+        this.fruits = [];
+        for (let type of Fruit.all) {
+            let fruit = {name: FruitType[type], score: type};
+            this.fruits.push(fruit);
+        }
+
         let board = this.board.nativeElement;
         this.gameService.init(board);
         this.gameService.newGame();
